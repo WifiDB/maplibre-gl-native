dff74c4bbc96984341ceb0fe7f8a91dc5251d27c
acalcutt <acalcutt@techidiots.net>
2023-02-26 16:49:31 -0500

    add rm to docker entrypoint file

--- platform/node/scripts/docker_build_entrypoint.sh
+++ platform/node/scripts/docker_build_entrypoint.sh
@@ -10,6 +10,7 @@ npm ci --ignore-scripts
 ccache --clear --set-config cache_dir=~/.ccache
 cmake . -B build -G Ninja  -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER=gcc-10 -DCMAKE_CXX_COMPILER=g++-10
 cmake --build build -j $(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null)
+rm -rf node_modules/
 
 #Test
 xvfb-run --auto-servernum ./build/mbgl-render-test-runner --manifestPath metrics/linux-gcc8-release-style.json
