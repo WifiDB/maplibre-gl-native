3f539364933c73659c92a5593d71c9b85af47fe1
acalcutt <acalcutt@techidiots.net>
2023-02-27 10:43:34 -0500

    put back old DockerFile/Entrypoint process

--- platform/node/scripts/docker_build_entrypoint.sh
+++ platform/node/scripts/docker_build_entrypoint.sh
@@ -5,11 +5,12 @@ cat /etc/os-release
 uname -m
 
 #Build lib files
-cd /data/
+cd /usr/src/app/
 npm ci --ignore-scripts
 ccache --clear --set-config cache_dir=~/.ccache
 cmake . -B build -G Ninja  -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER=gcc-10 -DCMAKE_CXX_COMPILER=g++-10
 cmake --build build -j $(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null)
+cp -R ./lib/ /data/lib/
 
 #Test
 xvfb-run --auto-servernum ./build/mbgl-render-test-runner --manifestPath metrics/linux-gcc8-release-style.json
